8e50205f12b39f2dad8913f7f9980c218a22e827
combs <ccombs@gmail.com>
2015-02-01 18:51:29 -0500

    minor cleanups

--- watchdog.h
+++ watchdog.h
@@ -16,12 +16,14 @@ void setup_watchdog(int timerPrescaler) {
   if (timerPrescaler > 9 ) timerPrescaler = 9; // Correct incoming amount if need be
 
   byte bb = timerPrescaler & 7; 
+
   if (timerPrescaler > 7) bb |= (1<<5); // Set the special 5th bit if necessary
+
 
 #if defined __AVR_ATmega328P__ || defined __AVR_ATmega168p__
 
   // ATmega328p or ATmega168p -- Arduino Uno, etc.
-  
+
   // This order of commands is important and cannot be combined
   MCUSR &= ~(1<<WDRF); // Clear the watch dog reset
   WDTCSR |= (1<<WDCE) | (1<<WDE); // Set WD_change enable, set WD enable
@@ -29,10 +31,11 @@ void setup_watchdog(int timerPrescaler) {
   WDTCSR |= _BV(WDIE); 
   // Set the interrupt enable, this will keep unit from resetting after each int
 
+
 #elif defined __AVR_ATtiny85__ || defined __AVR_ATtiny45__ || 
-	defined __AVR_ATtiny25__
- 
- // ATtiny85, 45, 25
+  defined __AVR_ATtiny25__
+
+    // ATtiny85, 45, 25
 
   // This order of commands is important and cannot be combined
   MCUSR &= ~(1<<WDRF); // Clear the watch dog reset
@@ -46,8 +49,9 @@ void setup_watchdog(int timerPrescaler) {
 #error "I don't know how to handle your AVR in setup_watchdog"
 #endif
 
-
 }
+
+
 
 #endif
 
