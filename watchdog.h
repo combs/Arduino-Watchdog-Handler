2797a088100fdfeccd8dcc59bed841429135d467
combs <ccombs@gmail.com>
2017-01-25 21:44:16 -0500

    more micros, text cleanup, Arduino 1.0+

--- watchdog.h
+++ watchdog.h
@@ -1,26 +1,29 @@
 // Helper for setting up watchdog interrupt function.
 
-// Define your interrupt routine in ISR(WDT_vect) { } 
+// Define your interrupt routine in ISR(WDT_vect) { }
 
-// Any variables used in your interrupt routine should be declared 
+// Any variables used in your interrupt routine should be declared
 // volatile to avoid sadness
 
 #ifndef WATCHDOG_H
 #define WATCHDOG_H
 
-#include <WProgram.h>
-
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
 
 void setup_watchdog(int timerPrescaler) {
 
   if (timerPrescaler > 9 ) timerPrescaler = 9; // Correct incoming amount if need be
 
-  byte bb = timerPrescaler & 7; 
+  byte bb = timerPrescaler & 7;
 
   if (timerPrescaler > 7) bb |= (1<<5); // Set the special 5th bit if necessary
 
 
-#if defined __AVR_ATmega328P__ || defined __AVR_ATmega168p__
+#if defined __AVR_ATmega328P__ || defined __AVR_ATmega168P__ || defined __AVR_ATmega168__ || defined __AVR_ATmega328__ || defined __AVR_ATmega328V__ || defined __AVR_ATmega168V__
 
   // ATmega328p or ATmega168p -- Arduino Uno, etc.
 
@@ -28,12 +31,11 @@ void setup_watchdog(int timerPrescaler) {
   MCUSR &= ~(1<<WDRF); // Clear the watch dog reset
   WDTCSR |= (1<<WDCE) | (1<<WDE); // Set WD_change enable, set WD enable
   WDTCSR = bb; // Set new watchdog timeout value
-  WDTCSR |= _BV(WDIE); 
+  WDTCSR |= _BV(WDIE);
   // Set the interrupt enable, this will keep unit from resetting after each int
 
 
-#elif defined __AVR_ATtiny85__ || defined __AVR_ATtiny45__ || 
-  defined __AVR_ATtiny25__
+#elif defined __AVR_ATtiny85__ || defined __AVR_ATtiny45__ ||   defined __AVR_ATtiny25__
 
     // ATtiny85, 45, 25
 
@@ -41,10 +43,10 @@ void setup_watchdog(int timerPrescaler) {
   MCUSR &= ~(1<<WDRF); // Clear the watch dog reset
   WDTCR |= (1<<WDCE) | (1<<WDE); // Set WD_change enable, set WD enable
   WDTCR = bb; // Set new watchdog timeout value
-  WDTCR |= _BV(WDIE); 
+  WDTCR |= _BV(WDIE);
   // Set the interrupt enable, this will keep unit from resetting after each int
 
-#else 
+#else
   //??
 #error "I don't know how to handle your AVR in setup_watchdog"
 #endif
@@ -54,5 +56,3 @@ void setup_watchdog(int timerPrescaler) {
 
 
 #endif
-
-
